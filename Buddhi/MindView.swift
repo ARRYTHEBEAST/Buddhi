448df1ef31136911e17b6847c43acca1876cd301
ARRYTHEBEAST <arjunkhamkar25@gmail.com>
2026-03-22 13:35:52 -0700

    MindView: Sanskrit text zooms in from center on black screen

--- Buddhi/MindView.swift
+++ Buddhi/MindView.swift
@@ -1,9 +1,30 @@
 import SwiftUI
 
 struct MindView: View {
+    @State private var textVisible = false
+
     var body: some View {
-        Color.black
-            .ignoresSafeArea()
+        ZStack {
+            Color.black.ignoresSafeArea()
+
+            VStack {
+                Text("Dharma Paramo Dharma")
+                    .font(.custom("Georgia-Italic", size: 22))
+                    .tracking(2)
+                    .foregroundColor(.white)
+                    .multilineTextAlignment(.center)
+                    .padding(.top, 72)
+                    .scaleEffect(textVisible ? 1 : 0.3)
+                    .opacity(textVisible ? 1 : 0)
+                    .animation(.timingCurve(0.22, 1, 0.36, 1, duration: 1.8).delay(0.2), value: textVisible)
+
+                Spacer()
+            }
+        }
+        .task {
+            try? await Task.sleep(nanoseconds: 16_000_000)
+            textVisible = true
+        }
     }
 }
 
